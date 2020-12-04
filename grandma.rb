ccbf21c9f6eb25bb8217b888e42b1ee0b089e800
Danny Brandt <dvbrandt90@gmail.com>
2020-12-04 14:44:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if string != string.upcase
+  if !string.upcase
     "HUH?! SPEAK UP, SONNY!"
     elsif string == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
